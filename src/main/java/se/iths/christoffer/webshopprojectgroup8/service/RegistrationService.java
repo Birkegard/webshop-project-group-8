fd8e8fb125f8b5263acef4771beec1860cd5994f
Birkegard <birkegard@hotmail.se>
2026-04-15 14:15:57 +0200

    Added methods to find a user, delete a user and send user information to RegistrationService

--- src/main/java/se/iths/christoffer/webshopprojectgroup8/service/RegistrationService.java
+++ src/main/java/se/iths/christoffer/webshopprojectgroup8/service/RegistrationService.java
@@ -1,7 +1,10 @@
 package se.iths.christoffer.webshopprojectgroup8.service;
 
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import se.iths.christoffer.springmessenger.model.Email;
+import se.iths.christoffer.springmessenger.service.MessageService;
 import se.iths.christoffer.webshopprojectgroup8.model.AppUser;
 import se.iths.christoffer.webshopprojectgroup8.repository.AppUserRepository;
 
@@ -9,10 +12,12 @@ import se.iths.christoffer.webshopprojectgroup8.repository.AppUserRepository;
 public class RegistrationService {
     private final AppUserRepository appUserRepository;
     private final PasswordEncoder encoder;
+    private final MessageService messageService;
 
-    public RegistrationService(AppUserRepository appUserRepository, PasswordEncoder encoder) {
+    public RegistrationService(AppUserRepository appUserRepository, PasswordEncoder encoder, MessageService messageService) {
         this.appUserRepository = appUserRepository;
         this.encoder = encoder;
+        this.messageService = messageService;
     }
 
     public AppUser registration(AppUser appUser) {
@@ -20,5 +25,24 @@ public class RegistrationService {
         appUser.setRole("USER");
         appUser.setPassword(encryptedPassword);
         return appUserRepository.save(appUser);
+    }
+
+    public AppUser getUserById(Long id) {
+        return appUserRepository.findById(id)
+                .orElseThrow(() -> new UsernameNotFoundException("Användare med id: " + id + " finns inte."));
+    }
+
+    public void deleteAppUser(Long id) {
+        appUserRepository.deleteById(id);
+    }
+
+    public void sendUserInformation(Long id) {
+        AppUser appUser = getUserById(id);
+
+        Email email = new Email();
+        email.setRecipient(appUser.getUsername());
+        email.setMessage("Username: " + appUser.getUsername() + " Role: " + appUser.getRole());
+        email.setSubject("Your user-details");
+        messageService.send(email);
     }
 }
