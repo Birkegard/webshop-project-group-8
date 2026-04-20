6c2d4d9a5c60849896c4f6297784c1c9cb01faf3
Ana-Ste <Ana.Stevanovic@iths.se>
2026-04-20 07:11:02 +0200

    Update WebshopProjectGroup8ApplicationTests.java

--- src/test/java/se/iths/christoffer/webshopprojectgroup8/WebshopProjectGroup8ApplicationTests.java
+++ src/test/java/se/iths/christoffer/webshopprojectgroup8/WebshopProjectGroup8ApplicationTests.java
@@ -3,15 +3,10 @@ package se.iths.christoffer.webshopprojectgroup8;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import se.iths.christoffer.webshopprojectgroup8.service.MailService;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class WebshopProjectGroup8ApplicationTests {
-
-    @MockBean
-    private MailService mailService;
 
     @Test
     void contextLoads() {
